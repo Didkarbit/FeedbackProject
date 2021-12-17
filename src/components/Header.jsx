cfa52fbdc82054cd010d0f6c1bc49e39b816ab9a
Didkarbit <powerfixprofi@gmail.com>
2021-12-17 16:43:53 +0200

    aded 3 components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
+import PropTypes from 'prop-types'
+    
+    
+export default function Header({ text, bgColor, textColor }) {
+    
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor
+}
 
-export default function Header({text}) {
     return (
-        <header>
+        <header style={headerStyles}>
             <div className="container">
                 <h2>
                     {text}
@@ -11,5 +19,15 @@ export default function Header({text}) {
     )
 }
 
+Header.defaultProps = {
+    text: 'Feedback UI',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95'
+}
 
+Header.propTypes = {
+    text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string
+}
 
